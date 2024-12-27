b0f99d44bc112c0a814faf345b7b5f8e7bc018f9
@_mathken <11295237+mathken029@users.noreply.github.com>
2024-12-27 13:26:38 +0900

    トップ画面にタイトルを表示して背景が映らないようにして、ARになったら消すようにしました

---
+++ Assets/ARShooting/Scripts/TitleBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace webxr_test.Scripts.ARShooting
+{
+    public class TitleBehaviour : MonoBehaviour
+    {
+        private void Start()
+        {
+            //UnityEditor上で実行している場合は自身を消滅させる
+            if (Application.isEditor)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
