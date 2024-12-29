77e3f4fee8db6c6861ec4f6ab08d54b3d943eb96
@_mathken <11295237+mathken029@users.noreply.github.com>
2024-12-29 18:31:25 +0900

    初期位置の正面に固定で敵が生成されるようにして、移動し続けないでいいようにしました

--- Assets/ARShooting/Scripts/EnemySpawnerBehaviour.cs
+++ Assets/ARShooting/Scripts/EnemySpawnerBehaviour.cs
@@ -12,25 +12,28 @@ namespace webxr_test.Scripts.ARShooting
         [SerializeField] private float spawnInterval = 1f;
         [SerializeField] private float spawnIntervalLast = 0.5f;
         
-        //敵を生成する距離
-        [SerializeField] private float spawnDistanceMin = 4f;
-        [SerializeField] private float spawnDistanceMax = 6f;
+        //プレイヤーの初期位置からどれくらい離れた場所に固定で敵を生成するか
+        [SerializeField] private float spawnDistance = 2f;
         
-        //敵を生成する高さ
-        [SerializeField] private float spawnHeightMin = -1f;
-        [SerializeField] private float spawnHeightMax = 1f;
+        //敵を生成するランダムな座標の範囲
+        [SerializeField] private float spawnRandomXMin = -1f;
+        [SerializeField] private float spawnRandomXMax = 1f;
+        [SerializeField] private float spawnRandomYMin = -1f;
+        [SerializeField] private float spawnRandomYMax = 1f;
+        [SerializeField] private float spawnRandomZMin = -1f;
+        [SerializeField] private float spawnRandomZMax = 1f;
         
         private PlayerBehaviour _playerBehaviour;
         
-        //プレイヤーの最初の位置のtransform
-        private Vector3 _playerStartPosition;
+        //敵を生成する位置の基準
+        private Vector3 _enemySpawnPosition;
         
         private void Start()
         {
             _playerBehaviour = FindObjectOfType<PlayerBehaviour>();
             
-            //プレイヤーの最初の位置を保存
-            _playerStartPosition = _playerBehaviour.transform.position;
+            //プレイヤーの初期位置から規定の居r離れた場所に敵を生成する場所を設定する
+            _enemySpawnPosition = _playerBehaviour.transform.position + _playerBehaviour.transform.forward * spawnDistance;
             
             //UnityEditor上で実行している場合は敵の生成を開始する
             if (Application.isEditor)
@@ -52,13 +55,12 @@ namespace webxr_test.Scripts.ARShooting
         //プレイヤーの初期位置から離れた位置で、上下左右の座標をランダムに敵を生成する
         private void SpawnEnemy()
         {
-            var playerTransform = _playerBehaviour.transform;
-            
-            //プレイヤーの初期位置から離れた位置で敵を生成する
-            var randomPosition = _playerStartPosition + playerTransform.forward * Random.Range(spawnDistanceMin, spawnDistanceMax);
-            
-            //上下もランダムにする
-            randomPosition += playerTransform.up * Random.Range(spawnHeightMin, spawnHeightMax);
+            //敵を生成する位置の基準からランダムな位置を設定する
+            var randomPosition = _enemySpawnPosition + new Vector3(
+                Random.Range(spawnRandomXMin, spawnRandomXMax),
+                Random.Range(spawnRandomYMin, spawnRandomYMax),
+                Random.Range(spawnRandomZMin, spawnRandomZMax)
+            );
             
             Instantiate(enemyPrefab, randomPosition, Quaternion.identity);
         }
