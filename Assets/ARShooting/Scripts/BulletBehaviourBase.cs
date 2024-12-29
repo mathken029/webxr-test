5504886d17dab5d15c8a1a65d40db0a24cc281d4
@_mathken <11295237+mathken029@users.noreply.github.com>
2024-12-29 19:52:50 +0900

    弾が二重でヒットしないようにしました

--- Assets/ARShooting/Scripts/BulletBehaviourBase.cs
+++ Assets/ARShooting/Scripts/BulletBehaviourBase.cs
@@ -11,14 +11,23 @@ public abstract class BulletBehaviourBase : MonoBehaviour
     //ダメージを与えたときの効果音の音量
     [SerializeField] protected float damageSoundVolume = 1f;
     
+    //コライダー
+    protected Collider _collider;
+    
     private void Start()
     {
+        //コライダーを取得
+        _collider = GetComponent<Collider>();
+        
         Destroy(gameObject, destroyTime);
     }
     
     //相手にヒットしたときの処理
     protected void Hit(Collider other)
     {
+        //弾の当たり判定をなくす
+        _collider.enabled = false;
+        
         AudioSource.PlayClipAtPoint(damageSound, transform.position, damageSoundVolume);
         Destroy(gameObject, damageSound.length);
     }
